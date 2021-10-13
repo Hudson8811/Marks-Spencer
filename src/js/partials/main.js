862e4a389f924b32b7272fc1bb6de42203ce6b76
Troshin Sergey <serge-troshin@yandex.ru>
2021-10-13 19:05:22 +0300

    styling desktop

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,3 +1,7 @@
-$(document).ready(function() {
+window.addEventListener('load', function() {
+  const sectionTexts = document.querySelectorAll('[class*="__section-text"]');
+  const sectionTitles = document.querySelectorAll('[class*="__section-title"]');
 
-})
+  //sectionTexts.forEach(it => it.setAttribute('contenteditable', true));
+  //sectionTitles.forEach(it => it.setAttribute('contenteditable', true));
+});
