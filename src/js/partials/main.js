bb86f355d057763b631669261769c3c74646f477
Troshin Sergey <serge-troshin@yandex.ru>
2021-10-15 10:04:21 +0300

    коммит

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,7 +1,10 @@
 window.addEventListener('load', function() {
-  const sectionTexts = document.querySelectorAll('[class*="__section-text"]');
-  const sectionTitles = document.querySelectorAll('[class*="__section-title"]');
+  //const sectionTexts = document.querySelectorAll('[class*="__section-text"]');
+  //const sectionTitles = document.querySelectorAll('[class*="__section-title"]');
 
   //sectionTexts.forEach(it => it.setAttribute('contenteditable', true));
   //sectionTitles.forEach(it => it.setAttribute('contenteditable', true));
+
+  AOS.init()
+
 });
